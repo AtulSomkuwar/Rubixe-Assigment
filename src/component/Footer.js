6b0bfec64153b82f74c7d0241c0bd22c6f984d85
Atul Sandeep Somkuwar <105080566+AtulSomkuwar@users.noreply.github.com>
2022-12-28 14:02:47 +0530

    Add files via upload

---
+++ src/component/Footer.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <div class="container">
+  
+  <div class="row">
+    <div class="col-4">
+    <h4>About us</h4>
+      <p>Rubixe™ is a global technology company specializing in disruptive technologies – Artificial Intelligence (AI), Machine Learning, Robotic Process Automation (RPA), BlockChain and Internet of Things (IoT). Rubixe mission to enable businesses to leverage the full potential of disruptive technologies to stay competitive in the market.</p>
+    </div>
+
+    <div class="col">
+    <h4>MENUS</h4>
+      <p>Home</p> 
+      <h6>Services</h6>
+       <h6>Products</h6>
+      <h6>Career</h6>
+    </div>
+
+    <div class="col">
+    <h4>LEARN MORE</h4>
+     <p>About</p>
+     <h6>Contact us</h6>
+    </div>
+
+    <div class="col">
+    <h4>ADRESS</h4>
+    <p>Novel Tech Park, 1st Floor, Hosur Rd,<br></br> Kudlu gate, Bengaluru, Karnataka<br></br> 560068
+Phone: 0804-717-8999<br></br>
+Email: hi@rubixe.com<br></br>
+SOCIAL MEDIA</p>
+
+    </div>
+  </div>
+</div>
+
+<div style={{backgroundColor:"black",color:"grey",textAlign:"center",height:"40px",paddingTop:"10px"}}>
+  <p>© Copyright 2017 - 2022 | Rubixe is a brand of THINK AHEAD INNOVATIONS PVT. LTD. | All Rights Reserved</p>
+</div>
+
+    </>
+  )
+}
+
+export default Footer
